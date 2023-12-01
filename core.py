5fb94bf7ae56d338d1284ad17158894616ff6ec5
Aditya Thute <aadityathute@gmail.com>
2023-12-02 00:02:23 +0530

    test1

--- core.py
+++ core.py
@@ -1,19 +1,19 @@
 # core.py
 
 import sys
+from main import arch_linux
 
-def main():
+def core():
     # Accessing command-line arguments
     os_arg = sys.argv[1] if len(sys.argv) > 1 else "unknown"
     distribution_arg = sys.argv[2] if len(sys.argv) > 2 else "unknown"
-    pkgmanager_arg = sys.argv[3] if len(sys.argv) > 2 else "unknown"
 
     if os_arg == "linux":
         # Function to detect Linux distribution
         if distribution_arg == "arch linux":
-            print(f"Detected Arch Linux....")
+            arch_linux()
     else:
         print(f"This script supports Arch Linux only.")
 
 if __name__ == "__main__":
-    main()
+    core()
