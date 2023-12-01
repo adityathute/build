bddef40a79479ecffb9aeed5538ca2ba13ff80e3
Aditya Thute <aadityathute@gmail.com>
2023-12-01 18:37:23 +0530

    Fix bugs

--- core.py
+++ core.py
@@ -5,20 +5,18 @@ import sys
 def main():
     # Accessing command-line arguments
     os_arg = sys.argv[1] if len(sys.argv) > 1 else "unknown"
-    distribution_arg = sys.argv[2] if len(sys.argv) > 2 else "unknown"
 
     if os_arg == "linux":
         # Function to detect Linux distribution
         detect_distribution()
-        print(f"OS: {os_arg}, Distribution: {distribution_arg}")
     elif os_arg == "macos":
         # Logic for macOS
-        print(f"OS: {os_arg}")
+        print(f"This script supports Arch Linux only. Detected {os_arg} operating system.")
     elif os_arg == "windows":
         # Logic for Windows
-        print(f"OS: {os_arg}")
+        print(f"This script supports Arch Linux only. Detected {os_arg} operating system.")
     else:
-        print(f"Unknown")
+        print(f"This script supports Arch Linux only.")
 
 def detect_distribution():
     # Accessing command-line arguments
@@ -26,15 +24,15 @@ def detect_distribution():
     pkgmanager_arg = sys.argv[3] if len(sys.argv) > 2 else "unknown"
 
     if distribution_arg == "arch":
-        print("Detected Arch Linux...." , {pkgmanager_arg})
+        print(f"Detected Arch Linux....{pkgmanager_arg}")
     elif distribution_arg in ["debian", "ubuntu", "linuxmint"]:
-        print(f"Detected {distribution_arg} distribution....")
+        print(f"This script supports Arch Linux only. Detected {distribution_arg} distribution.")
     elif distribution_arg in ["fedora", "centos", "rhel"]:
-        print(f"Detected {distribution_arg} distribution....")
+        print(f"This script supports Arch Linux only. Detected {distribution_arg} distribution.")
     elif distribution_arg in ["opensuse", "suse"]:
-        print(f"Detected {distribution_arg} distribution....")
+        print(f"This script supports Arch Linux only. Detected {distribution_arg} distribution.")
     else:
-        print(f"Unknown distribution....")
+        print("This script supports Arch Linux only.")
 
 if __name__ == "__main__":
     main()
