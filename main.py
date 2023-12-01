254c13ccdf03e52909b62968b763be334de286a6
Aditya Thute <aadityathute@gmail.com>
2023-12-02 02:19:01 +0530

    Configure aliases and install packages for Arch Linux.

--- main.py
+++ main.py
@@ -1,11 +1,98 @@
 # main.py
 
-def set_alias(distro):
-    print(f"This is set_alias with {distro}.")
+import os
+import subprocess
+import shutil
 
+# Implement alias setting based on distro and OS.
+def set_alias(distro, operating_system):
+
+    # Get the path to the script's directory
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Specify the path to the alias.txt and .bashrc files relative to the script's directory
+    alias_file_path = os.path.join(script_directory, f"os/{operating_system}/{distro}/alias.txt")
+    bashrc_file_path = os.path.join(os.path.expanduser("~"), ".bashrc")
+
+    # Check if the file exists before attempting to open it
+    if os.path.exists(alias_file_path):
+        # Read the content of alias.txt
+        with open(alias_file_path, 'r') as alias_file:
+            alias_content = alias_file.read()
+
+        # Check if the content is already in .bashrc
+        with open(bashrc_file_path, 'r') as bashrc_file:
+            bashrc_content = bashrc_file.read()
+
+        # Find the range of content from # Aliases to # end_alises in .bashrc
+        start_marker = "# Aliases"
+        end_marker = "# end_alises"
+        start_index = bashrc_content.find(start_marker)
+        end_index = bashrc_content.find(end_marker) + len(end_marker)
+
+        # Check if the range is found
+        if start_index != -1 and end_index != -1:
+            # Replace the existing range with the new aliases
+            updated_bashrc_content = f"{bashrc_content[:start_index]}# Aliases\n{alias_content}\n# end_alises{bashrc_content[end_index:]}"
+        else:
+            # Add the new aliases to the end of the file
+            updated_bashrc_content = f"{bashrc_content}\n# Aliases\n{alias_content}\n# end_alises"
+
+        # Write the updated content to .bashrc
+        with open(bashrc_file_path, 'w') as bashrc_file:
+            bashrc_file.write(updated_bashrc_content)
+
+        print(f"Aliases added successfully.")
+
+        # Source the updated .bashrc file
+        source_command = f"source {bashrc_file_path}"
+        # You may want to run this command in a subprocess or shell
+        subprocess.run(source_command, shell=True)
+    else:
+        print(f"File not found: {alias_file_path}")
+
+# Check package is already installed
+def is_package_installed(package_name, pkg_manager):
+    # Check if a package is installed using Pacman on Arch Linux.
+    try:
+        subprocess.run([pkg_manager, "-Q", package_name], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+# Install essential packages
 def install_packages(pkg_manager):
-    print(f"This is install_packages with {pkg_manager}.")
+    install_pkgs_command = f"sudo {pkg_manager} -S --noconfirm firefox nodejs npm mariadb-libs mariadb base-devel"
+    subprocess.run(install_pkgs_command, shell=True)
 
+    # Check if 'yay' is installed
+    if not is_package_installed("yay", pkg_manager):
+
+        # Check if yay folder exists and remove it
+        if os.path.exists("yay"):
+            shutil.rmtree("yay")
+
+        # Clone yay repository
+        subprocess.run(["git", "clone", "https://aur.archlinux.org/yay.git"], check=True)
+
+        # Change directory to yay
+        os.chdir("yay")
+
+        # Build and install yay
+        subprocess.run(["makepkg", "-si", "--noconfirm"], check=True)
+
+        # Install additional packages using yay
+        subprocess.run(["yay", "-S", "visual-studio-code-bin", "--noconfirm"], check=True)
+
+        # Change back to the original directory
+        os.chdir("..")
+
+        # Remove yay folder
+        shutil.rmtree("yay")
+
+    print(f"System packages installed successfully.")
+
+# Configure aliases and install packages for an Arch Linux environment.
 def arch_linux():
-    set_alias("arch_linux")
+    set_alias("arch_linux", "linux")
     install_packages("pacman")
